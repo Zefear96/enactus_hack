03b953090451aac8d05306c72b88fbf18b0e0bdc
Zamir <eazzy_freez@mail.ru>
2023-04-13 19:20:30 +0600

    add nav

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,8 @@
 import Navbar from "@/components/Navbar";
-import { HeaderSearch } from "@/components/NavbarMantine";
 
 export default function Home() {
 	return (
 		<>
-			{/* <HeaderSearch {links = [
-				{
-					link: "Services",
-					label: 'Services'
-				}
-			] /> */}
 			<Navbar />
 		</>
 	);
