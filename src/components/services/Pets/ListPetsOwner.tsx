39953aada671ee2ec9711cde2b258b66868361af
Zamir <eazzy_freez@mail.ru>
2023-04-22 19:57:45 +0600

    add details styl

--- src/components/services/Pets/ListPetsOwner.tsx
+++ src/components/services/Pets/ListPetsOwner.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import ItemPet from "./ItemPet";
+import React, { useEffect, useState } from "react";
+import ItemPetOwner from "./ItemPetOwner";
 import Link from "next/link";
 import {
 	Group,
@@ -16,18 +16,29 @@ import { setPage } from "@/store/slices/petsFilters.slice";
 import { useFetchPets } from "@/services/pets/fetchPets";
 import { useEditPet } from "@/services/pets/editPet";
 import { useFetchUser } from "@/services/user/fetchUser";
+import { Pet } from "@/utils/types";
 
 const ListPetsOwner = () => {
 	const [currentUser] = useFetchUser();
-
 	const dispatch = useAppDispatch();
 	const petsFilters = useAppSelector((state) => state.petsFilters);
+	console.log(petsFilters);
+	const [pets, { isLoading }] = useFetchPets(petsFilters);
+	const [ownerList, setOwnerList] = useState<Pet[]>([]);
+
+	useEffect(() => {
+		if (pets) {
+			setOwnerList(pets.filter((pet) => pet.owner === currentUser?.email));
+		}
+	}, []);
+
+	// setOwnerList(pets?.filter((pet) => pet.owner === currentUser?.email));
+	console.log(ownerList);
 
 	const handleChange = (page: number) => {
 		dispatch(setPage(page));
 	};
 
-	const [pets, { isLoading }] = useFetchPets(petsFilters);
 	console.log(pets);
 
 	if (isLoading) {
@@ -44,19 +55,17 @@ const ListPetsOwner = () => {
 
 	return (
 		<div className="flex-col w-3/4 mx-auto my-10">
-			<div className=" grid grid-cols-2 gap-10 w-3/4 mx-auto ">
-				{pets.results
-					.filter((item) => item.owner === currentUser?.email)
-					.map((item) => (
-						<ItemPet item={item} key={item.id} />
-					))}
+			<div className=" grid grid-cols-1 gap-10 w-3/4 mx-auto ">
+				{ownerList.map((item) => (
+					<ItemPetOwner item={item} key={item.id} />
+				))}
 			</div>
 			<Group position="center" className="my-20">
 				<Pagination
 					value={petsFilters.page ?? 1}
 					onChange={handleChange}
 					siblings={1}
-					total={Math.ceil(pets.count / petsFilters._limit)}
+					total={Math.ceil((ownerList.length ?? 0) / petsFilters._limit)}
 				/>
 			</Group>
 		</div>
