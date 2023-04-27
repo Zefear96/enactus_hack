560c0d73a82160efb87c362882e2109b3c351519
Zamir <eazzy_freez@mail.ru>
2023-04-27 11:25:49 +0600

    fix bug

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ const nextConfig = {
 	reactStrictMode: true,
 	images: {
 		domains: ["34.89.184.22"],
+		// domains: ["petshackaton.ru"],
 	},
 	i18n: {
 		locales: ["ru", "kg"],
 		defaultLocale: "ru",
-	}
+	},
 };
 
 module.exports = nextConfig;
-
