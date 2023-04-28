b3a529d7ffa48dd8f8a64c07dbad2e6b8667f2d0
Zamir <eazzy_freez@mail.ru>
2023-04-29 00:16:59 +0600

    fix bugs

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 // export const API_URL = "http://34.89.184.22";
-export const API_URL = "https://petshackaton.ru";
+export const API_URL = "https://petshackaton.ru/";
 
 // export const MEDIA_URL = "http://34.89.184.22";
 // export const API_GEO = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${position.coords.latitude}&lon=${position.coords.longitude}`;
