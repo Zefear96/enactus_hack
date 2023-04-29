742b6daf62dfb0c1013960ad3a7cb41ed18674de
Zamir <eazzy_freez@mail.ru>
2023-04-30 00:15:50 +0600

    fix comm list adapt

--- src/components/services/commercial/ListHostels.tsx
+++ src/components/services/commercial/ListHostels.tsx
@@ -1,38 +1,50 @@
-import React from 'react';
-import ItemCommercial from './ItemCommercial';
+import React from "react";
+import ItemCommercial from "./ItemCommercial";
 import Image from "next/image";
-import textad from '../../../../public/textad.png';
-import { useFetchHostels } from '@/services/commercials/fetchCommCategories';
+import textad from "../../../../public/textad.png";
+import { useFetchHostels } from "@/services/commercials/fetchCommCategories";
 
 const ListHostels = () => {
-    const [data] = useFetchHostels();
-    console.log(data);
+	const [data] = useFetchHostels();
+	console.log(data);
 
-    return (
-        <div>
-            <div className=" flex justify-between flex-wrap w-full mx-auto my-7">
-                {data.map((item) => (
-                    <ItemCommercial item={item} key={item.id} />
-                ))}
-            </div>
-            <div style={{
-                backgroundImage: "url(" + "https://i.ibb.co/W30Mqf7/yourad.png" + ")", width: '100%', height: '500px', margin: 'auto', display: 'flex', alignItems: 'center', flexDirection: 'column',
-                backgroundSize: 'auto',
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center',
-                backgroundColor: 'rgb(240, 123, 194)'
-            }}>
-                <Image src={textad} alt="error :(" />
-                <button className="bg-bluelogin text-yellowlogin w-60 mb-4 rounded-3xl h-10 relative z-[1] hover:bg-yellowlogin hover:text-bluelogin"
-                    onMouseOver={(e) => {
-                        e.currentTarget.style.boxShadow = "10px 10px 0px 4px #988CE1";
-                    }}
-                    onMouseOut={(e) => {
-                        e.currentTarget.style.boxShadow = "none";
-                    }}>Подать рекламу</button>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div>
+			<div className=" flex justify-between flex-wrap w-full mx-auto my-7 max-sm:flex-col">
+				{data.map((item) => (
+					<ItemCommercial item={item} key={item.id} />
+				))}
+			</div>
+			<div
+				style={{
+					backgroundImage: "url(" + "https://i.ibb.co/W30Mqf7/yourad.png" + ")",
+					width: "100%",
+					height: "500px",
+					margin: "auto",
+					display: "flex",
+					alignItems: "center",
+					flexDirection: "column",
+					backgroundSize: "auto",
+					backgroundRepeat: "no-repeat",
+					backgroundPosition: "center",
+					backgroundColor: "rgb(240, 123, 194)",
+				}}
+			>
+				<Image src={textad} alt="error :(" />
+				<button
+					className="bg-bluelogin text-yellowlogin w-60 mb-4 rounded-3xl h-10 relative z-[1] hover:bg-yellowlogin hover:text-bluelogin"
+					onMouseOver={(e) => {
+						e.currentTarget.style.boxShadow = "10px 10px 0px 4px #988CE1";
+					}}
+					onMouseOut={(e) => {
+						e.currentTarget.style.boxShadow = "none";
+					}}
+				>
+					Подать рекламу
+				</button>
+			</div>
+		</div>
+	);
+};
 
-export default ListHostels
+export default ListHostels;
